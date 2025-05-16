d899e218e94e935b608c55abb1c91ce2d8ef0737
bani <dalllo9851@gmail.com>
2025-05-16 21:49:50 +0900

    [8일차] 선택정렬

---
+++ src/main/java/org/example/codeingTest/내림차순자릿수정렬_선택정렬.java
@@ -0,0 +1,32 @@
+package org.example.codeingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 내림차순자릿수정렬_선택정렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] arr = new int[str.length()];
+        for (int i=0; i<str.length(); i++) {
+            arr[i] = Integer.parseInt(str.substring(i,i+1));
+        }
+
+        for(int i=0; i<str.length(); i++) {
+            int MAX = i;
+            for(int j=i+1; j<str.length(); j++) {
+                if(arr[j] > arr[MAX]) {
+                    MAX = j;
+                }
+            }
+            if(arr[i] < arr[MAX]) {
+                int tmp = arr[i];
+                arr[i] = arr[MAX];
+                arr[MAX] = tmp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
