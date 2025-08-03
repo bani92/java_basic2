664ef98251e74c99fbefba790f72711c900cdc9a
bani <dalllo9851@gmail.com>
2025-08-03 18:22:27 +0900

    [코딩테스트] 백준 2문제 BFS

---
+++ src/main/java/org/example/codeingTest/BFS/유기농배추.java
@@ -0,0 +1,78 @@
+package org.example.codeingTest.BFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 유기농배추 {
+    static boolean[][] visited;
+    static int[][] map;          // 배추 위치 저장용
+    static int M, N;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken()); // 가로
+            N = Integer.parseInt(st.nextToken()); // 세로
+            int K = Integer.parseInt(st.nextToken());
+
+            map = new int[N][M];
+            visited = new boolean[N][M];
+            for(int j=0; j<K; j++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[y][x] = 1;
+            }
+
+            int wormCount = 0;
+            for(int x1=0; x1<M; x1++) {
+                for(int y1=0; y1<N; y1++) {
+                    if(!visited[y1][x1] && map[y1][x1] == 1) {
+                        bfs(y1, x1);
+                        wormCount++;
+                    }
+                }
+            }
+
+            sb.append(wormCount).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void bfs(int y1, int x1) {
+
+        Queue<int[]> queue = new LinkedList<>();
+        visited[y1][x1] = true;
+        queue.add(new int[]{x1, y1});
+
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int currentX = current[0];
+            int currentY = current[1];
+
+            for(int i=0; i<4; i++) {
+                int nx = currentX + dx[i];
+                int ny = currentY + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= M || ny >= N) {
+                    continue;
+                }
+
+                if(!visited[ny][nx] && map[ny][nx] == 1) {
+                    visited[ny][nx] = true;
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/codeingTest/BFS/촌수계산.java
@@ -0,0 +1,72 @@
+package org.example.codeingTest.BFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 촌수계산 {
+    static ArrayList<ArrayList<Integer>> graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int startNum = Integer.parseInt(st.nextToken());
+        int endNum = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        for(int i=0; i<=N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        visited = new boolean[N+1];
+        int M = Integer.parseInt(br.readLine());
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph.get(x).add(y);
+            graph.get(y).add(x);
+        }
+
+        int result = bfs(startNum, endNum);
+        System.out.println(result);
+    }
+
+    public static int bfs(int start, int end) {
+
+        // 7을 넣어서 7번째 배열을 확인한다.
+        // 해당 배열의 item들을 큐에 넣는다.
+        // 큐에 빼면서 해당 List에 접근한다.
+
+
+        Queue<int[]> queue = new LinkedList<>();
+        visited[start] = true;
+        queue.add(new int[]{start, 0});
+
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int currentNum = current[0];
+            int distance = current[1];
+
+            // 확인
+            if(currentNum == end) {
+                return distance;
+            }
+
+            for(int item : graph.get(currentNum)) {
+                if(!visited[item]) {
+                    visited[item] = true;
+                    queue.add(new int[]{item, distance + 1});
+                }
+            }
+
+        }
+        return -1;
+    }
+}
