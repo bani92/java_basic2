78a88a6c82535a7692c646b82d7823cb908a5708
bani <dalllo9851@gmail.com>
2025-08-12 21:55:09 +0900

    [코딩테스트] 백준 1문제 (버블,선택정렬)

---
+++ src/main/java/org/example/codeingTest/bronze/소트인사이드.java
@@ -0,0 +1,36 @@
+package org.example.codeingTest.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 소트인사이드 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int A[] = new int[str.length()];
+        for(int i=0; i<str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i,i+1));
+        }
+
+        // 선택정렬
+        for(int i=0; i<str.length(); i++) {
+            int Max = i;
+            for(int j=i+1; j<str.length(); j++) {
+                if(A[j] > A[Max]) {
+                    Max = j;
+                }
+            }
+            if(A[i] < A[Max]) {
+                int tmp = A[i];
+                A[i] = A[Max];
+                A[Max] = tmp;
+            }
+        }
+        for(int i=0; i<str.length(); i++) {
+            System.out.print(A[i]);
+        }
+
+
+    }
+}
---
+++ src/main/java/org/example/codeingTest/bronze/수정렬하기1.java
@@ -0,0 +1,33 @@
+package org.example.codeingTest.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 수정렬하기1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int A[] = new int[N];
+
+        for(int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 버블 정렬 구현
+        for(int i=0; i<N-1; i++) {
+            for(int j=0; j<N-1-i; j++) {
+                if(A[j]>A[j+1]) {
+                    int temp = A[j];
+                    A[j] = A[j+1];
+                    A[j+1] = temp;
+                }
+            }
+        }
+
+        for(int i=0; i<N; i++) {
+            System.out.println(A[i]);
+        }
+
+
+    }
+}
