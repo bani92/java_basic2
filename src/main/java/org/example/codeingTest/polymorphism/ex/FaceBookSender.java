18ff8b92dcb0cedcfe417bcfb0ad2fea7b94927b
bani <dalllo9851@gmail.com>
2025-08-25 22:44:36 +0900

    [코딩테스트] 백트래킹 1문제, 다형성

---
+++ src/main/java/org/example/codeingTest/polymorphism/ex/FaceBookSender.java
@@ -0,0 +1,8 @@
+package org.example.codeingTest.polymorphism.ex;
+
+public class FaceBookSender implements Sender {
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("페이스북 메시지를 발송합니다. " + message);
+    }
+}
---
+++ src/main/java/org/example/codeingTest/polymorphism/pay1/KakaoPay.java
@@ -0,0 +1,15 @@
+package org.example.codeingTest.polymorphism.pay1;
+
+public class KakaoPay implements Pay{
+
+    public KakaoPay() {
+        System.out.println("카카오페이 시스템과 연결합니다.");
+    }
+
+    @Override
+    public boolean pay(int amount) {
+        System.out.println(amount + "원 결제를 시도합니다.");
+        return true;
+    }
+
+}
